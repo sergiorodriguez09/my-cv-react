b2951c45f6de6a40097ca6c0539afd713de779db
Sergio Rodriguez <sergio.rolo94@gmail.com>
2021-11-03 09:25:22 +0100

    First commit

---
+++ src/CV/CV.js
@@ -0,0 +1,95 @@
+export const CV = {
+    hero: {
+      name: "Sergio",
+      adress: "Rodríguez López",
+      city: "Madrid",
+      email: "sergio.rdz.lpz@hotmail.com",
+      birthDate: "15/02/1994",
+      phone: "695048512",
+      image: "https://avatars.githubusercontent.com/u/91475456?v=4",
+      gitHub: "https://github.com/sergiorodriguez09",
+      aboutMe: [
+        {
+          info: "💌 Amante de la vida.",
+        },
+        {
+          info: "🎾 Intento de jugador de tenis",
+        },
+        {
+          info: "🍔 Comer es mi hobby favorito.",
+        },
+        {
+          info: "🖥️ Estudiante de programación",
+        },
+      ],
+    },
+    education: [
+      {
+        name: "Desarrollo Full Stack",
+        date: "2021",
+        where: "Upgrade Hub",
+      },
+      {
+        name: "Curso de Marketing Digital para RRSS",
+        date: "2021",
+        where: "Agencia SEO",
+      },
+      {
+        name: "Grado Superior en Gestión de Alojamientos Turísticos",
+        date: "2019-2021",
+        where: "ITEP",
+      },
+      {
+        name: "Grado Superior en Sonido",
+        date: "2016-2018",
+        where: "IES Puerta Bonita",
+      },
+    ],
+    experience: [
+      {
+        name: "Agente inmobiliario",
+        date: "01/11/2021 – 01/09/2021",
+        where: "REMAX",
+        description:
+          "Venta y alquiler de propiedades",
+      },
+      {
+        name: "Recepcionista",
+        date: "01/11/2019 – 31/10/2020",
+        where: "Victory House London Hotel",
+        description:
+          "Gestión de habitaciones y tareas varias",
+      },
+      {
+        name: "Camarero",
+        date: "01/02/2019 – 31/10/2019",
+        where: "The Conduit London",
+      },
+      {
+        name: "Teleoperador",
+        date: "01/01/2018 – 01/01/2019",
+        where: "Universidad CEU San Pablo",
+      },
+    ],
+    languages: {
+      language: "Inglés",
+      wrlevel: "Alto",
+      splevel: "Alto",
+    },
+    habilities: ["HTML5", "CSS3", "Git", "Angular", "React", "MongoDB", "Node", "PHP"],
+    volunteer: [
+      {
+        name: "Centro de Mayores Boadilla del Monte",
+        where: "Madrid",
+        description:
+          "Semana en la que estuve yendo varios días al centro para ayudar a personas mayores",
+      },
+      {
+        name: "Quita-nieves",
+        where: "Madrid",
+        description:
+          "Ayudé a quitar nieve de las calles durante la catástrofe de Filomena",
+      },
+    ],
+  };
+  
---
+++ src/components/More.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./More.css";
+
+const More = ({ languages, habilities, volunteer }) => {
+  return (
+    <div className="more">
+      <div className="divider">
+        <span></span>
+        <span>Idiomas</span>
+        <span></span>
+      </div>
+      <div className="card"> 
+        <p>{languages.language}</p>
+        <p>Nivel escrito: ✏️   {languages.wrlevel}</p>
+        <p>Nivel oral: 🗣️{languages.splevel}</p>
+      </div>
+        <div className="divider">
+        <span></span>
+        <span>Skills</span>
+        <span></span>
+        </div>
+      <div className="habilities card">
+        {habilities.map((txt) => (
+          <p>✅{txt}</p>
+        ))}
+      </div>
+      <div className="divider">
+        <span></span>
+        <span>Voluntariados</span>
+        <span></span>
+      </div>
+      <div className="volunteer card">
+      {volunteer.map((item) => {
+          return (
+            <div key={JSON.stringify(item)}>
+              <p className="name">⭕ {item.name}</p>
+              <p>{item.where}</p>
+              <p>{item.description}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default More;
